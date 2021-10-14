7847d04318fbb532f0f1d2f8892de32fbef49a2c
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-10-14 17:05:12 +0200

    Challenge 2 completed
    updated structure and functions
    a diceroll
    has to pick a low and high

--- Session2/minigame/src/main/kotlin/Main.kt
+++ Session2/minigame/src/main/kotlin/Main.kt
@@ -14,13 +14,42 @@ fun challengeOne(){
     val userAnswer  = readLine()
 
     if(answers.contains(userAnswer)){
-        win()
+        challengeTwo()
         }else{
             gameOver()
         }
 }
+
+fun challengeTwo(){
+    println("A huge cloud of Mosquitos are following you, you see a bit further a river to which you can run to and probably" +
+            "dive into it and hold your breath until they leave")
+    println("Pick low or high")
+    diceRoll()
+}
+fun diceRoll(){
+    val dice = (1..6).shuffled().first()
+
+
+    val userAnswer = readLine()
+
+    var acceptedAnswers = setOf<Int>()
+
+    if(userAnswer == "low" ){
+        acceptedAnswers = setOf<Int>(1,2,3)
+    }else{
+        acceptedAnswers = setOf<Int>(4,5,6)
+    }
+    if(acceptedAnswers.contains(dice)){
+            println("You have safely made it into the water")
+        }else{
+            println("You where too slow and you died because the musquitos sucked all your blood")
+    }
+            println("you have rolled  $dice")
+}
+
 fun win(){
     println("Congratz!")
+    challengeTwo()
 }
 
 fun gameOver(){
