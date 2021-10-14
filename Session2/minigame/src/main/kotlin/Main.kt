cf54d6a3142a26a6d33d6055d4a2a06a5e1e4334
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-10-14 15:36:55 +0200

    challenge one finalized
    the introduction and challenge where he has to choose a weapon of choisc to defend himself from the animals

--- Session2/minigame/src/main/kotlin/Main.kt
+++ Session2/minigame/src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
 fun main(){
-    println("Hello world")
+    println("Welcome adventure to your story, you are currently traveling through one of the deadliest places of the earth." +
+            "Depending on your choices we will see if you can survive long enough to get back home safely ")
+    challengeOne()
+
+}
+
+fun challengeOne(){
+    println("Intro challenge 1")
+    println("There is a pack of wild animals running at you please select one of the weapons to defend yourself!")
+
+    val answers = setOf<String>("gun", "sword", "shield", "knives", "bow")
+    println(answers)
+    val userAnswer  = readLine()
+
+    if(answers.contains(userAnswer)){
+        win()
+        }else{
+            gameOver()
+        }
+}
+fun win(){
+    println("Congratz!")
+}
+
+fun gameOver(){
+    println("You are eaten by the lions")
 }
