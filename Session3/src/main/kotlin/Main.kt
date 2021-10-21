f493f7a338b6cafc87a5f1c2d7871e80e6b3ee77
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-10-21 15:01:48 +0200

    gitignore added

---
+++ Session3/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+          fun main(){
+              val design1 = Module(
+                  "Design 1",
+                  "Some info",
+                  1,
+                  1,
+                  5,
+                  false,
+                  "Design",
+                  arrayOf("PS", "Illustrator"),
+                  arrayOf("PS, IL"),
+
+                  "Maike",
+
+
+              )
+          }
---
+++ Session3/src/main/kotlin/Module.kt
@@ -0,0 +1,15 @@
+class Module(
+    val title : String,
+    val description: String,
+    val semester: Int,
+    val year: Int,
+    val credits: Int,
+    val optional: Boolean,
+    val name: String,
+    val language: Array<String>,
+    val software: Array<String>,
+    val teacher: String,
+) {
+
+
+    }
