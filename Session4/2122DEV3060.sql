b0a9d25b83ab9c90db952f073c58a02a2c509496
jeroenver22 <jeroenver22@users.noreply.github.com>
2021-10-28 17:25:17 +0200

    Create 2122DEV3060.sql
    sql database for trains and trainrides

---
+++ Session4/2122DEV3060.sql
@@ -0,0 +1,73 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `train_id` int(11) DEFAULT NULL,
+  `departure_city_id` int(11) DEFAULT NULL,
+  `destination_city_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `passagers_ride` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `passenger_id` int(11) DEFAULT NULL,
+  `train_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `passengers` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `rides` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `rides` (
+  `id` int(11) NOT NULL,
+  `departure_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `train_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `trains` (
+  `id` int(255) NOT NULL,
+  `type` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `capacity` int(255) NOT NULL,
+  `max_speed` int(255) DEFAULT NULL,
+  `max_passangers` int(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+INSERT INTO `passagers_ride` (`id`, `passenger_id`, `train_id`) VALUES
+(1, 1, 1);
+INSERT INTO `passagers_ride` (`id`, `passenger_id`, `train_id`) VALUES
+(2, 1, 2);
+
+
+INSERT INTO `passengers` (`id`, `rides`) VALUES
+(1, NULL);
+
+
+
+
+
+
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
