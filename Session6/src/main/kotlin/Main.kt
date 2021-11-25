2fe64e0a14280eed9c943228fb136d79ce8f7162
Jeroen Vermeiren <jeroen.vermeiren@student.ehb.be>
2021-11-25 17:48:30 +0100

    Update Main.kt
    first exercise finished except for the part where it shows the first train that is going to arrive, it shows all trains

--- Session6/src/main/kotlin/Main.kt
+++ Session6/src/main/kotlin/Main.kt
@@ -13,10 +13,27 @@ fun main(){
     val connection =  DriverManager.getConnection(
         "jdbc:" + "mysql" + "://" +
                 credentials.hostName +
-                ":" + "3306 (this is your port, this is the default)" + "/" +
+                ":" + "3306" + "/" +
                 credentials.databaseName,
         connectionProps)
+    val search = println("To what city would you like to go ?")
+    val input = readLine()
+    val statement = connection.prepareStatement("SELECT departure_time from rides \n" +
+    "INNER JOIN cities on rides.train_city_id = cities.departure_city_id \n" +
+            "WHERE cities.`name`= ?")
+    // Replace the var without allowing full queries to be entered
+        statement.setString(1, input)
+    val result = statement.executeQuery()
+    while(result.next()) {
+        result.getString("departure_time")
+        println("Your train is leaving at: " + result.getString( "departure_time"))
+    }
 
 
+
+
+
+}
+fun askCity(){
 
 }
